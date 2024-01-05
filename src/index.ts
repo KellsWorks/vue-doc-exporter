5ea6bf2249633e8f14c44ddd211af8d10eccf068
kellworks <kchidothi@outlook.com>
2024-01-05 12:33:19 +0200

    fix: update module exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import ExportToWord from "./components/ExportToWord.vue";
 import ExportToCsv from "./components/ExportToCsv.vue";
 import ExportToExcel from "./components/ExportToExcel.vue";
+import ExportToPdf from "./components/ExportToPdf.vue";
 
-export { ExportToWord, ExportToExcel, ExportToCsv }
+export { ExportToWord, ExportToExcel, ExportToCsv, ExportToPdf }
