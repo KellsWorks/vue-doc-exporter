926b37920a20ce81d5a1479fe3f1535e0c599bb7
kellworks <kchidothi@outlook.com>
2024-08-29 10:28:21 +0200

    refactor: index.ts module exports

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import ExportToWord from "./components/ExportToWord.vue";
 import ExportToCsv from "./components/ExportToCsv.vue";
 import ExportToExcel from "./components/ExportToExcel.vue";
 import ExportToPdf from "./components/ExportToPdf.vue";
+import { ExportPDFTable } from "./components/exportPDFtable";
 
-export { ExportToWord, ExportToExcel, ExportToCsv, ExportToPdf }
+export { ExportToWord, ExportToExcel, ExportToCsv, ExportToPdf, ExportPDFTable };
